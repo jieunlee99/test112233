da36482befdb517eb3b8d9c8befe95d6329540a1
Jinseok Sim <jinseok@simjinseok.com>
2024-09-14 18:01:20 +0900

    auth 모듈 추가

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReviewsModule } from './reviews/reviews.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ReviewsModule],
+  imports: [ReviewsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
