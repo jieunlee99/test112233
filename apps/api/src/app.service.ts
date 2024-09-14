67e485075cf7742ba45bbeb7398ca7bfe45b7fb3
Jinseok Sim <jinseok@simjinseok.com>
2024-09-14 18:01:42 +0900

    카테고리 가져오기 예제 추가

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {createClient} from '@supabase/supabase-js'
+
+import {QueryResult, QueryData, QueryError} from '@supabase/supabase-js'
+
+const supabase = createClient('https://algsfyxfsvqgthqbmwzr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsZ3NmeXhmc3ZxZ3RocWJtd3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYxMTEyODgsImV4cCI6MjA0MTY4NzI4OH0.7Y2QUFRraSGmt3NWbSGSUflMx71kjxWCVo8jA5EWjII')
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!!';
-  }
+    getHello(): string {
+        return 'Hello World!!';
+    }
+
+    async getCategories() {
+        const categoriesQuery = supabase
+            .from("categories")
+            .select(`id, name`);
+        // type CountriesWithCities = QueryData<typeof countriesWithCitiesQuery>;
+
+        const {data, error} = await categoriesQuery;
+        if (error) throw error;
+        return data;
+        // const countriesWithCities: CountriesWithCities = data;
+    }
 }
