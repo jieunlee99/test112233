b32829d90c6a077a40f53ab4dab5671ad7b66868
Jinseok Sim <jinseok@simjinseok.com>
2024-09-14 18:00:28 +0900

    cors 정책 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, {
+        cors: {
+            origin: 'http://localhost:5173',
+        }
+    });
+    await app.listen(3000);
 }
+
 bootstrap();
