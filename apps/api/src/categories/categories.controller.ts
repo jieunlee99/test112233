b5471c2c66e30832653d72be49729010f625f141
Jinseok Sim <jinseok@simjinseok.com>
2024-09-17 20:37:13 +0900

    포맷 설정

--- apps/api/src/categories/categories.controller.ts
+++ apps/api/src/categories/categories.controller.ts
@@ -1,79 +1,77 @@
-import {Controller, UseGuards, Get, Post, Put, Delete, Param, Body, Res, NotFoundException} from '@nestjs/common';
-import {ApiTags, ApiBearerAuth, ApiOperation, ApiParam, ApiBody, ApiResponse, ApiProperty} from '@nestjs/swagger'
-import {Response} from 'express';
+import { Controller, UseGuards, Get, Post, Put, Delete, Param, Body, Res, NotFoundException } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiOperation, ApiParam, ApiBody, ApiResponse, ApiProperty } from '@nestjs/swagger';
+import { Response } from 'express';
 
-import {AuthGuard} from "../auth/auth.guard";
-import {CategoriesService} from './categories.service';
-import {GetCurrentUser} from "../auth/auth.decorator";
+import { AuthGuard } from '../auth/auth.guard';
+import { CategoriesService } from './categories.service';
+import { GetCurrentUser } from '../auth/auth.decorator';
 
 namespace CategoryDTO {
-    export class Create {
-        @ApiProperty({description: '카테고리명', default: '카테고리1'})
-        name: string;
+  export class Create {
+    @ApiProperty({ description: '카테고리명', default: '카테고리1' })
+    name: string;
 
-        @ApiProperty({description: '카테고리 순서', default: 1})
-        sort_order: number;
+    @ApiProperty({ description: '카테고리 순서', default: 1 })
+    sort_order: number;
 
-        @ApiProperty({description: '카테고리 공개 범위', default: 'private'})
-        visibility: "public" | "private";
-    }
+    @ApiProperty({ description: '카테고리 공개 범위', default: 'private' })
+    visibility: 'public' | 'private';
+  }
 }
 
 @ApiBearerAuth()
 @Controller('categories')
 @ApiTags('카테고리')
 export class CategoriesController {
-    constructor(private readonly categoriesService: CategoriesService) {
-    }
+  constructor(private readonly categoriesService: CategoriesService) {
+  }
 
-    @Get()
-    @UseGuards(AuthGuard)
-    @ApiOperation({summary: '카테고리 목록', description: '카테고리를 가져옵니다'})
-    @ApiResponse({status: 200, description: '카테고리를 가져왔습니다'})
-    @ApiResponse({status: 401, description: '로그인이 필요합니다'})
-    async getList(@GetCurrentUser() user) {
-        return await this.categoriesService.getList(user);
-    }
+  @Get()
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: '카테고리 목록', description: '카테고리를 가져옵니다' })
+  @ApiResponse({ status: 200, description: '카테고리를 가져왔습니다' })
+  @ApiResponse({ status: 401, description: '로그인이 필요합니다' })
+  async getList(@GetCurrentUser() user) {
+    return await this.categoriesService.getList(user);
+  }
 
-    @Post()
-    @UseGuards(AuthGuard)
-    @ApiOperation({summary: '카테고리 생성', description: '카테고리를 생성합니다'})
-    @ApiBody({type: CategoryDTO.Create})
-    @ApiResponse({status: 201, description: '카테고리를 생성하였습니다 '})
-    @ApiResponse({status: 401, description: '로그인이 필요합니다'})
-    @ApiResponse({status: 422, description: '유효하지 않은 데이터'})
-    async create(
+  @Post()
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: '카테고리 생성', description: '카테고리를 생성합니다' })
+  @ApiBody({ type: CategoryDTO.Create })
+  @ApiResponse({ status: 201, description: '카테고리를 생성하였습니다 ' })
+  @ApiResponse({ status: 401, description: '로그인이 필요합니다' })
+  @ApiResponse({ status: 422, description: '유효하지 않은 데이터' })
+  async create(
         @GetCurrentUser() user,
-        @Body() body: CategoryDTO.Create
-    ) {
-        return await this.categoriesService.create(user, body);
-    }
+        @Body() body: CategoryDTO.Create,
+  ) {
+    return await this.categoriesService.create(user, body);
+  }
 
-    @Put(':id')
-    @ApiOperation({summary: '카테고리 수정', description: '카테고리를 수정합니다'})
-    async update() {
+  @Put(':id')
+  @ApiOperation({ summary: '카테고리 수정', description: '카테고리를 수정합니다' })
+  async update() {
 
-    }
+  }
 
-    @Delete(":id")
-    @UseGuards(AuthGuard)
-    @ApiParam({name: 'id', required: true})
-    @ApiResponse({status: 204, description: '카테고리를 삭제하였습니다'})
-    @ApiResponse({status: 401, description: '로그인이 필요합니다'})
-    @ApiResponse({status: 404, description: '존재하지 않는 카테고리'})
-    async remove(
+  @Delete(':id')
+  @UseGuards(AuthGuard)
+  @ApiParam({ name: 'id', required: true })
+  @ApiResponse({ status: 204, description: '카테고리를 삭제하였습니다' })
+  @ApiResponse({ status: 401, description: '로그인이 필요합니다' })
+  @ApiResponse({ status: 404, description: '존재하지 않는 카테고리' })
+  async remove(
         @Res() response: Response,
         @GetCurrentUser() user,
         @Param('id') id: number,
-    ) {
-        const result = await this.categoriesService.delete(user, {id});
+  ) {
+    const result = await this.categoriesService.delete(user, { id });
 
-        if (!result) {
-            throw new NotFoundException();
-        }
-
-        return response.status(204).send(null);
+    if (!result) {
+      throw new NotFoundException();
     }
+
+    return response.status(204).send(null);
+  }
 }
-
-
--- apps/api/src/categories/categories.service.ts
+++ apps/api/src/categories/categories.service.ts
@@ -1,50 +1,50 @@
-import {Injectable} from '@nestjs/common';
-import {createClient} from '@supabase/supabase-js';
+import { Injectable } from '@nestjs/common';
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient('https://algsfyxfsvqgthqbmwzr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsZ3NmeXhmc3ZxZ3RocWJtd3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYxMTEyODgsImV4cCI6MjA0MTY4NzI4OH0.7Y2QUFRraSGmt3NWbSGSUflMx71kjxWCVo8jA5EWjII')
+const supabase = createClient('https://algsfyxfsvqgthqbmwzr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsZ3NmeXhmc3ZxZ3RocWJtd3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYxMTEyODgsImV4cCI6MjA0MTY4NzI4OH0.7Y2QUFRraSGmt3NWbSGSUflMx71kjxWCVo8jA5EWjII');
 
 @Injectable()
 export class CategoriesService {
-    async getList(user) {
-        const {
-            data,
-            error
-        } = await supabase
-            .from('categories')
-            .select(`
+  async getList(user) {
+    const {
+      data,
+      error,
+    } = await supabase
+      .from('categories')
+      .select(`
             id,
             name,
             sort_order
             `)
-            .eq('user_id', user.id)
-            .is('deleted_at', null)
-            .order('sort_order', {ascending: true});
-        return data;
+      .eq('user_id', user.id)
+      .is('deleted_at', null)
+      .order('sort_order', { ascending: true });
+    return data;
+  }
+
+  async create(user, { name, sort_order, visibility = 'private' }) {
+    const { data, error } = await supabase
+      .from('categories')
+      .insert({
+        user_id: user.id,
+        name,
+        sort_order,
+        visibility,
+      })
+      .select(`id,name,sort_order,visibility`);
+
+    return data;
+  }
+
+  async delete(user, { id }) {
+    const {
+      data,
+      error,
+    } = await supabase.from('categories').update({ deleted_at: new Date() }).eq('id', Number(id)).eq('user_id', user.id).is('deleted_at', null).select();
+
+    if (data.length > 0) {
+      return true;
     }
-
-    async create(user, {name, sort_order, visibility = 'private'}) {
-        const {data, error} = await supabase
-            .from('categories')
-            .insert({
-                user_id: user.id,
-                name,
-                sort_order,
-                visibility
-            })
-            .select(`id,name,sort_order,visibility`);
-
-        return data;
-    }
-
-    async delete(user, {id}) {
-        const {
-            data,
-            error
-        } = await supabase.from('categories').update({deleted_at: new Date()}).eq('id', Number(id)).eq('user_id', user.id).is('deleted_at', null).select();
-
-        if (data.length > 0) {
-            return true;
-        }
-        return error;
-    }
+    return error;
+  }
 }
